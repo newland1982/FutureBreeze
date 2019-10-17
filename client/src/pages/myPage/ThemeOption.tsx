28160daad21a8b7e8b93a522690ff505f2c6ec37
ope50089 <tamanegee2000@yahoo.co.jp>
2019-10-18 02:15:27 +0900

    Update client file ThemeOption.tsx

--- client/src/pages/myPage/ThemeOption.tsx
+++ client/src/pages/myPage/ThemeOption.tsx
@@ -3,14 +3,16 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Icon from '@mdi/react';
+import IconButton from '@material-ui/core/IconButton';
 import Menu from '../../components/Menu';
 import React, { useContext, Fragment } from 'react';
-// import TextField from '@material-ui/core/TextField';
-import themeStore from '../../data/themeStore';
+import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
+import themeStore from '../../data/themeStore';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { mdiBackspace } from '@mdi/js';
 import { mdiChevronLeft } from '@mdi/js';
 import { mdiChevronRight } from '@mdi/js';
 import { useTheme } from '@material-ui/core/styles';
@@ -25,8 +27,23 @@ const useStyles = makeStyles((theme: Theme) =>
       [theme.breakpoints.up('sm')]: { marginTop: 0 },
       [theme.breakpoints.down('xs')]: { marginTop: 0 }
     },
+    searchBox: {
+      maxWidth: 200,
+      margin: '0 auto'
+    },
     iconBox: {
       margin: '0 auto'
+    },
+    chevronLeftIcon: {
+      position: 'fixed',
+      top: 'calc((100vh - 48px) / 2)'
+      // right: theme.spacing(2)
+    },
+    chevronRightIcon: {
+      position: 'fixed',
+      top: 'calc((100vh - 48px) / 2)',
+
+      right: 0
     },
     gridList: {
       [theme.breakpoints.up('xs')]: { width: 600 },
@@ -35,8 +52,8 @@ const useStyles = makeStyles((theme: Theme) =>
       [theme.breakpoints.up('lg')]: { width: 1280 },
       [theme.breakpoints.up('xl')]: { width: 1280 },
       height: 'auto',
-      paddingRight: 24,
-      paddingLeft: 24,
+      paddingRight: 36,
+      paddingLeft: 36,
       paddingTop: 12,
       paddingBottom: 48
     },
@@ -82,6 +99,49 @@ const ThemeOption = () => {
   return (
     <Fragment>
       <Menu />
+      <Box className={classes.chevronLeftIcon}>
+        <IconButton size='small'>
+          <Icon
+            path={mdiChevronLeft}
+            size={1}
+            color='#FFF'
+            style={{
+              cursor: 'pointer'
+            }}
+          />
+        </IconButton>
+      </Box>
+      <Box className={classes.chevronRightIcon}>
+        <IconButton size='small'>
+          <Icon
+            path={mdiChevronRight}
+            size={1}
+            color='#FFF'
+            style={{
+              cursor: 'pointer'
+            }}
+          />
+        </IconButton>
+      </Box>
+      {/* <Box mt={3} mb={2}>
+        <Toolbar variant='dense'>
+          <Box className={classes.searchBox}>
+            <TextField
+              margin='dense'
+              variant='outlined'
+              inputProps={{ 'aria-label': 'bare' }}
+            />
+            <Icon
+              path={mdiBackspace}
+              size={1}
+              color='#FFF'
+              style={{
+                cursor: 'pointer'
+              }}
+            />
+          </Box>
+        </Toolbar>
+      </Box> */}
       <div className={classes.root}>
         <GridList
           spacing={6}
@@ -90,31 +150,20 @@ const ThemeOption = () => {
           cols={cols()}
         >
           <GridListTile cols={cols()} style={{ height: 'auto' }}>
-            {/* <TextField
+            <TextField
+              className={classes.searchBox}
               margin='dense'
               variant='outlined'
               inputProps={{ 'aria-label': 'bare' }}
-            /> */}
-            <Toolbar variant='dense'>
-              <Box className={classes.iconBox}>
-                <Icon
-                  path={mdiChevronLeft}
-                  size={1}
-                  color='#FFF'
-                  style={{
-                    cursor: 'pointer'
-                  }}
-                />
-                <Icon
-                  path={mdiChevronRight}
-                  size={1}
-                  color='#FFF'
-                  style={{
-                    cursor: 'pointer'
-                  }}
-                />
-              </Box>
-            </Toolbar>
+            />
+            <Icon
+              path={mdiBackspace}
+              size={1}
+              color='#FFF'
+              style={{
+                cursor: 'pointer'
+              }}
+            />
           </GridListTile>
           {tileData.map(tile => (
             <GridListTile
