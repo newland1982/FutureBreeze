527fb582652d18e8ecce44e4c43f78109d53fa97
craigrance <tamanegee2000@yahoo.co.jp>
2020-04-13 00:34:10 +0900

    Update client file App.css

--- client/src/style/theme.ts
+++ client/src/style/theme.ts
@@ -3,34 +3,34 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   mixins: {
     toolbar: {
-      minHeight: 56
-    }
+      minHeight: 56,
+    },
   },
   palette: {
     primary: {
-      main: 'rgba(0, 0, 0, 0.36)'
+      main: 'rgba(0, 0, 0, 0.36)',
     },
     secondary: {
-      main: 'rgba(0, 0, 0, 0.6)'
+      main: 'rgba(0, 0, 0, 0.6)',
     },
     text: {
       primary: '#fff',
-      secondary: '#fff'
+      secondary: '#fff',
     },
     background: {
       paper: 'rgba(0, 0, 0, 0.6)',
-      default: '#000'
+      default: '#000',
     },
     action: {
       disabled: 'rgba(255, 255, 255, 0.36)',
-      disabledBackground: 'rgba(255, 255, 255, 0.36)'
-    }
+      disabledBackground: 'rgba(255, 255, 255, 0.36)',
+    },
   },
   typography: {
     button: {
-      textTransform: 'none'
-    }
-  }
+      textTransform: 'none',
+    },
+  },
 });
 
 export default theme;
