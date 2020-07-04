035dedc3e64cdc6bd6e0f7291dad7b4bada1d23d
craigrance <tamanegee2000@yahoo.co.jp>
2020-07-04 19:50:37 +0900

    Update cloud file adjustScreens.js

--- cloud/screen/adjustScreens.js
+++ cloud/screen/adjustScreens.js
@@ -180,3 +180,22 @@ exports.handler = async (event) => {
     }
   })();
 };
+
+/* layer package.json
+{
+  "dependencies": {
+    "apollo-cache-inmemory": "^1.1.0",
+    "apollo-client": "^2.0.3",
+    "apollo-link": "^1.0.3",
+    "apollo-link-http": "^1.2.0",
+    "aws-sdk": "^2.141.0",
+    "aws-appsync": "^1.0.0",
+    "es6-promise": "^4.1.1",
+    "graphql": "^0.11.7",
+    "graphql-tag": "^2.5.0",
+    "isomorphic-fetch": "^2.2.1",
+    "ws": "^3.3.1",
+    "amazon-cognito-identity-js": "^3.2.0"
+  }
+}
+*/
