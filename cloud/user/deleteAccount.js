a21357543b7ca20c936902c8830d285f2c32cd01
craigrance <tamanegee2000@yahoo.co.jp>
2020-07-26 14:05:06 +0900

    Add cloud file deleteAccount.js

---
+++ cloud/user/deleteAccount.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// @ts-ignore
+global.WebSocket = require('ws');
+// @ts-ignore
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+// @ts-ignore
+const AUTH_TYPE = require('aws-appsync/lib/link/auth-link').AUTH_TYPE;
+// @ts-ignore
+const AWSAppSyncClient = require('aws-appsync').default;
+// @ts-ignore
+const AWS = require('aws-sdk');
+// @ts-ignore
+const gql = require('graphql-tag');
+const credentials = AWS.config.credentials;
+let cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = (event, context, callback) => {
+  event.Records.forEach((record) => {
+    if (
+      record.eventName !== 'ObjectCreated:Put' &&
+      record.eventName !== 'ObjectCreated:CompleteMultipartUpload'
+    ) {
+      return;
+    }
+
+    (async () => {})();
+  });
+};
+
+/* layer package.json
+{
+  "dependencies": {
+    "apollo-cache-inmemory": "^1.1.0",
+    "apollo-client": "^2.0.3",
+    "apollo-link": "^1.0.3",
+    "apollo-link-http": "^1.2.0",
+    "aws-sdk": "^2.141.0",
+    "aws-appsync": "^1.0.0",
+    "es6-promise": "^4.1.1",
+    "graphql": "^0.11.7",
+    "graphql-tag": "^2.5.0",
+    "isomorphic-fetch": "^2.2.1",
+    "ws": "^3.3.1"
+  }
+}
+*/
